4ce7e6a0fe907eef86bc8ec84f25330e9c56398d
mateusnobre <mateusnobrests@gmail.com>
2021-07-03 12:00:04 -0300

    colors setup

---
+++ src/components/Colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    purple: '#8a5eff',
+    purpleLight: '#b18fdf',
+    mainBlack: '#333333',
+    productBackground: '#dee5e5',
+    orange: '#fe5131',
+    green: '#d8ff6e'
+}
+
+export default colors
